feab244e92a1b1ac7589c2205c4e9671098a0519
Cameron Craig <sentrathis96@gmail.com>
2016-05-06 15:11:22 -0700

    Add nokogiri to gemfile / tweaks to convert method

--- lib/actionmailer-text/html_to_plain_text.rb
+++ lib/actionmailer-text/html_to_plain_text.rb
@@ -5,11 +5,12 @@ module ActionMailer
     module HtmlToPlainText
       def convert_to_text(html_body)
         body_parts = []
-        Nokogiri::HTML(html_part[:body]).traverse do |node|
-          if node.text? and !(content = node.content ? node.content.strip : nil).blank?
+        Nokogiri::HTML(html_body).traverse do |node|
+          if node.text? and !(content = node.content ? node.content.strip : nil).empty?
             body_parts << content
           elsif node.name == "a" && (href = node.attr("href")) && href.match(/^https?:/)
             body_parts << href
+          end
         end
         body_parts.uniq.join("\n")
       end
